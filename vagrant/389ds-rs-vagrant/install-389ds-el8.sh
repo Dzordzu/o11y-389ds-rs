08ffe1ec6688c5c70938926e46c26e414f613759
Tomasz Durda <tomekdur@wp.pl>
2026-01-19 22:15:31 +0100

    vagrant fixes

--- vagrant/389ds-rs-vagrant/install-389ds-el8.sh
+++ vagrant/389ds-rs-vagrant/install-389ds-el8.sh
@@ -3,5 +3,9 @@ dnf config-manager --set-enabled powertools
 dnf copr enable @389ds/389-directory-server -y
 dnf install 389-ds-base cockpit-389-ds vim -y
 
-dscreate from-file /vagrant/dscreate.ini
-systemctl enable --now dirsrv@default
+IS_READY=$(dsctl -l | wc -l)
+
+if [[ "$IS_READY" -lt 1 ]]; then
+   dscreate from-file /vagrant/dscreate.ini
+   systemctl enable --now dirsrv@default
+fi
--- vagrant/389ds-rs-vagrant/install-haproxy.sh
+++ vagrant/389ds-rs-vagrant/install-haproxy.sh
@@ -1,4 +1,6 @@
-dnf install haproxy vim -y
-cp /vagrant/ldap-haproxy.cfg /etc/haproxy/conf.d/ldap.cfg
+#!/bin/bash
+dnf install haproxy vim policycoreutils-python-utils -y
+cp /vagrant/ldap-haproxy.cfg /etc/haproxy/haproxy.cfg
+setsebool -P haproxy_connect_any 1
 systemctl enable --now haproxy
 systemctl restart haproxy
